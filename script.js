5d135ac3142d21253ce798ce2201acf003fc0b9e
PremMallela <86968287+PremMallela@users.noreply.github.com>
2024-02-11 20:25:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const questions =[
     ]
    },
    {
-    question : "What is the smallest coninent in the world?",
+    question : "What is the smallest continent in the world?",
     answers :[
       { text :"Asia",correct :false },
       { text :"Australia",correct :true },
@@ -55,11 +55,11 @@ function startQuiz(){
 function showQuestion(){
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNumber = currentQuestionIndex+1;
+    let questionNumber = currentQuestionIndex+ 1;
     questionElement.innerHTML = questionNumber +". "+ currentQuestion.question;
 
     let correctAnswer;
-    for(var i=0; i<currentQuestion.answers.length;i++){
+    for(var i=0; i< currentQuestion.answers.length; i++){
         if(currentQuestion.answers[i].correct === true ){
             correctAnswer = currentQuestion.answers[i].text;
             break;
@@ -71,17 +71,44 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
+
         button.addEventListener("click",(event)=>{
+          console.log("Button clicked-1!");
            let selectedBtn = event.target;
            let answerChoosen = selectedBtn.textContent;
+
            if(answerChoosen === correctAnswer ){
-                selectedBtn.classList.add("correct");
+                selectedBtn.className = "btn correct";
+                score++;
            }
            else{
-               selectedBtn.classList.add("Incorrect");
+               selectedBtn.classList.add("incorrect");
            }
-    })
+           //Disable the buttons for the rest of the course.
+           const disableButton = document.querySelectorAll(".btn");
+           disableButton.forEach(btn => {
+            btn.disabled = true;
+           });
+
+           //add the next button again
+           nextButton.style.display ="block";
+           
+           //proceeds to next Question
+           currentQuestionIndex++;
+           
+       });
 })
+      nextButton.addEventListener("click",()=>{
+         if(currentQuestionIndex > (questions.length-1)){
+              resetState();
+               const h2= document.querySelector("h2");
+               h2.innerHTML = "Quiz Score - " + score+ "/"+ questions.length;
+               h2.style.textAlign ="center";
+           }
+          else{
+            showQuestion();   
+          }
+         })
 };
 
 function resetState(){
